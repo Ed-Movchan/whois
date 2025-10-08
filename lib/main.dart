0dc281919f22e2ce9de09f422a262f6388a40ad4
Eduard <movchan@jacquet.nl>
2025-10-08 15:06:54 +0200

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class WhoisCheckerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: WhoisHomePage(),
       theme: ThemeData(
         primarySwatch: Colors.green,
